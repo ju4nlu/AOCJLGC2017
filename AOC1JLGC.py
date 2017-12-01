e8a9620a35d8edae4b5e0a45a2dfa8d38e55cf36
ju4nlu <juanluisgomezchanclon@gmail.com>
2017-12-01 12:50:00 +0100

    Updated challenge 1
    comments added

--- AOC1JLGC.py
+++ AOC1JLGC.py
@@ -1,27 +1,29 @@
-
 import sys
 
+# Checking the input is right
 if len(sys.argv) != 2:
     print("A parameter specifying the .txt directory is required!")
     sys.exit()
 
+# Getting that file openned
 with open(sys.argv[1], 'r') as f:
     input_captcha = f.read()
 f.closed
 
-#input_captcha = "123341"
-
+# Needed variables to keep track
 previous_digit = 0
 counter = 0
 
+# Main loop. Just checks if the current digit is equal to the previous one
+# if this is true, then the digit is added to the counter
 for digit in input_captcha:
     if digit.isdigit():
         if digit==previous_digit:
             counter += int(digit)
         previous_digit = digit
 
+# As the list is circular, we must check the first against the last element
 if input_captcha[0] == previous_digit:
     counter += int(input_captcha[0])
-    
-        
+       
 print ("The solution to the captcha is... {}".format(counter))
