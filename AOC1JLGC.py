4cabbae3d251e16560785d26c40efa8b97d00807
ju4nlu <juanluisgomezchanclon@gmail.com>
2017-12-01 13:12:09 +0100

    updated challenge 1
    added second part of the challenge

--- AOC1JLGC.py
+++ AOC1JLGC.py
@@ -10,6 +10,8 @@ with open(sys.argv[1], 'r') as f:
     input_captcha = f.read()
 f.closed
 
+################# FIRST PART #################
+
 # Needed variables to keep track
 previous_digit = 0
 counter = 0
@@ -26,4 +28,24 @@ for digit in input_captcha:
 if input_captcha[0] == previous_digit:
     counter += int(input_captcha[0])
        
-print ("The solution to the captcha is... {}".format(counter))
+print ("The solution to the FIRST captcha is:\t{}".format(counter))
+
+################# SECOND PART #################
+
+# Now we also need the distance between elements
+next_digit = 0
+counter = 0
+d = len(input_captcha)/2
+
+# As we need to check the elements at distance d of each one, we need
+# to know if we are in the lower or in the upper part of the sequence
+for i, digit in enumerate(input_captcha):
+    if i<d:
+        next_digit = input_captcha[i+d]
+    else:
+        next_digit = input_captcha[i-d]
+    
+    if digit == next_digit:
+        counter += int(digit)
+        
+print ("The solution to the SECOND captcha is:\t{}".format(counter))
